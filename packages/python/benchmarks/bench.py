4958eb44ae603b552c2dcb61260d078927cbfaa6
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2025-12-25 17:48:46 +0100

    Add benchmarks for Python bindings

---
+++ packages/python/benchmarks/bench.py
@@ -0,0 +1,33 @@
+import pytest
+import random
+from qlit import CliffordTCircuit, simulate_circuit, simulate_circuit_parallel1, simulate_circuit_parallel, simulate_circuit_parallel2
+
+target = simulate_circuit_parallel2
+
+def setup(qubits, gates, t_gates):
+    seed = 123
+    random.seed(seed)
+    w = format(random.getrandbits(qubits), f"00{qubits}b")
+    circuit = CliffordTCircuit.random(qubits, gates, t_gates, seed)
+    return w, circuit
+
+@pytest.mark.parametrize("qubits", [10, 100, 1000])
+def test_qubits(benchmark, qubits):
+    gates = 1000
+    t_gates = 5
+    w, circuit = setup(qubits, gates, t_gates)
+    benchmark(target, w, circuit)
+
+@pytest.mark.parametrize("gates", [100, 1000, 10_000])
+def test_gates(benchmark, gates):
+    qubits = 100
+    t_gates = 5
+    w, circuit = setup(qubits, gates, t_gates)
+    benchmark(target, w, circuit)
+
+@pytest.mark.parametrize("t_gates", [0, 5, 10])
+def test_t_gates(benchmark, t_gates):
+    qubits = 100
+    gates = 1000
+    w, circuit = setup(qubits, gates, t_gates)
+    benchmark(target, w, circuit)
