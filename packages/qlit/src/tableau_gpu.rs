c1d188aef7ed767a4f3cbad6cc3cc97df8c616bc
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2026-01-30 22:19:12 +0100

    Print adapter info

--- packages/qlit/src/tableau_gpu.rs
+++ packages/qlit/src/tableau_gpu.rs
@@ -49,6 +49,7 @@ impl GpuContext {
         let instance = wgpu::Instance::new(&Default::default());
         let adapter = instance.request_adapter(&Default::default()).await.unwrap();
         let (device, queue) = adapter.request_device(&Default::default()).await.unwrap();
+        println!("Using WGPU adapter: {:?}", adapter.get_info());
 
         let shader_module = device.create_shader_module(wgpu::include_wgsl!("tableau.wgsl"));
 
