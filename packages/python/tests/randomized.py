77852cdc610d6bc515fce53f82f266acc2c12673
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2026-02-12 16:28:59 +0100

    Synchronize tests across implementations in Python

--- packages/python/tests/randomized.py
+++ packages/python/tests/randomized.py
@@ -1,61 +1,108 @@
 import random
 import unittest
-from qlit import CliffordTCircuit, CliffordTGate, simulate_circuit_gpu
+
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
+from qlit import (
+    CliffordTCircuit,
+    CliffordTGate,
+    simulate_circuit,
+    simulate_circuit_gpu,
+    simulate_circuit_hybrid,
+)
 
 
 class QlitRandomizedTests(unittest.TestCase):
-    def test_random(self):
-        iterations = 1000
+    def setup(
+        self, qubits, gates, t_gates, seed
+    ) -> tuple[CliffordTCircuit, QuantumCircuit, str]:
+        circuit = CliffordTCircuit.random(qubits, gates, t_gates, seed)
+        random.seed(seed)
+        w = format(random.getrandbits(qubits), f"00{qubits}b")
+
+        qiskit_circuit = QuantumCircuit(qubits)
+        for gate in circuit.gates:
+            match gate:
+                case CliffordTGate.X(a):
+                    qiskit_circuit.x(a)
+                case CliffordTGate.Y(a):
+                    qiskit_circuit.y(a)
+                case CliffordTGate.Z(a):
+                    qiskit_circuit.z(a)
+                case CliffordTGate.H(a):
+                    qiskit_circuit.h(a)
+                case CliffordTGate.S(a):
+                    qiskit_circuit.s(a)
+                case CliffordTGate.Sdg(a):
+                    qiskit_circuit.sdg(a)
+                case CliffordTGate.Cnot(a, b):
+                    qiskit_circuit.cx(a, b)
+                case CliffordTGate.Cz(a, b):
+                    qiskit_circuit.cz(a, b)
+                case CliffordTGate.T(a):
+                    qiskit_circuit.t(a)
+                case CliffordTGate.Tdg(a):
+                    qiskit_circuit.tdg(a)
+                case _:
+                    raise ValueError(f"Unknown gate: {gate}")
+
+        return circuit, qiskit_circuit, w
+
+    def test_against_statevector(self):
+        print("\nTesting against Qiskit statevector simulator...")
+        iterations = 500
         for i in range(iterations):
             qubits = 10
             t_gates = 5
             gates = 100
             seed = 1234 + i
-            circuit = CliffordTCircuit.random(qubits, gates, t_gates, seed)
-            random.seed(seed)
-            w = format(random.getrandbits(qubits), f"00{qubits}b")
-
-            # qiskit
-            qiskit_circuit = QuantumCircuit(qubits)
-            for gate in circuit.gates:
-                match gate:
-                    case CliffordTGate.X(a):
-                        qiskit_circuit.x(a)
-                    case CliffordTGate.Y(a):
-                        qiskit_circuit.y(a)
-                    case CliffordTGate.Z(a):
-                        qiskit_circuit.z(a)
-                    case CliffordTGate.H(a):
-                        qiskit_circuit.h(a)
-                    case CliffordTGate.S(a):
-                        qiskit_circuit.s(a)
-                    case CliffordTGate.Sdg(a):
-                        qiskit_circuit.sdg(a)
-                    case CliffordTGate.Cnot(a, b):
-                        qiskit_circuit.cx(a, b)
-                    case CliffordTGate.Cz(a, b):
-                        qiskit_circuit.cz(a, b)
-                    case CliffordTGate.T(a):
-                        qiskit_circuit.t(a)
-                    case CliffordTGate.Tdg(a):
-                        qiskit_circuit.tdg(a)
-                    case _:
-                        raise ValueError(f"Unknown gate: {gate}")
+            circuit, qiskit_circuit, w = self.setup(qubits, gates, t_gates, seed)
 
             state = Statevector.from_instruction(qiskit_circuit)
             # Remember that Qiskit uses reversed basis states
             w_be = w[::-1]
             w_coeff_qiskit = state.data[int(w_be, 2)]
 
-            # qlit
-            w_coeff_qlit = simulate_circuit_gpu(w, circuit)
+            self.assertAlmostEqual(
+                simulate_circuit(w, circuit),
+                w_coeff_qiskit,
+                msg=f"\nCPU: {w=} \n{qiskit_circuit} \n{circuit.gates=}",
+            )
+            self.assertAlmostEqual(
+                simulate_circuit_gpu(w, circuit),
+                w_coeff_qiskit,
+                msg=f"\nGPU: {w=} \n{qiskit_circuit} \n{circuit.gates=}",
+            )
+            self.assertAlmostEqual(
+                simulate_circuit_hybrid(w, circuit),
+                w_coeff_qiskit,
+                msg=f"\nHybrid: {w=} \n{qiskit_circuit} \n{circuit.gates=}",
+            )
+            print("✅", end="", flush=True)
+
+    def test_implementations_against_eachother(self):
+        print("\nTesting that all implementations give same result...")
+        iterations = 10
+        for i in range(iterations):
+            qubits = 130
+            t_gates = 3
+            gates = 1000
+            seed = 1234 + i
+            circuit, qiskit_circuit, w = self.setup(qubits, gates, t_gates, seed)
+
+            simulate_circuit_res = simulate_circuit(w, circuit)
+            simulate_circuit_gpu_res = simulate_circuit_gpu(w, circuit)
+            simulate_circuit_hybrid_res = simulate_circuit_hybrid(w, circuit)
 
             self.assertAlmostEqual(
-                w_coeff_qlit,
-                w_coeff_qiskit,
-                msg=f"\n{w=} \n{state.data=} \n{qiskit_circuit} \n{circuit.gates=}",
+                simulate_circuit_res,
+                simulate_circuit_gpu_res,
+                msg=f"\nCPU/GPU: {w=} \n{qiskit_circuit} \n{circuit.gates=}",
+            )
+            self.assertAlmostEqual(
+                simulate_circuit_gpu_res,
+                simulate_circuit_hybrid_res,
+                msg=f"\nCPU/Hybrid: {w=} \n{qiskit_circuit} \n{circuit.gates=}",
             )
             print("✅", end="", flush=True)
 
--- packages/python/tests/test.py
+++ packages/python/tests/test.py
@@ -1,9 +1,18 @@
-from math import sqrt
 import unittest
-from qlit import simulate_circuit, CliffordTCircuit, CliffordTGate
+from math import sqrt
+
+from qlit import (
+    CliffordTCircuit,
+    CliffordTGate,
+    simulate_circuit,
+    simulate_circuit_gpu,
+    simulate_circuit_hybrid,
+)
+
+DELTA = 1e-6
 
 
-class QlitPythonTests(unittest.TestCase):
+class CircuitTests(unittest.TestCase):
     def test_invalid_qubit_index(self):
         self.assertRaises(ValueError, CliffordTCircuit, 8, [CliffordTGate.X(165)])
         self.assertRaises(ValueError, CliffordTCircuit, 8, [CliffordTGate.X(8)])
@@ -18,19 +27,18 @@ class QlitPythonTests(unittest.TestCase):
         self.assertRaises(ValueError, CliffordTCircuit, 8, [CliffordTGate.Cnot(4, 8)])
         self.assertRaises(ValueError, CliffordTCircuit, 8, [CliffordTGate.Cz(8, 4)])
         self.assertRaises(ValueError, CliffordTCircuit, 8, [CliffordTGate.Cz(4, 8)])
-    
 
+
+class CpuTests(unittest.TestCase):
     def test_mismatched_qubit_number(self):
         circuit = CliffordTCircuit(8, [])
         w = "000000000"
         self.assertRaises(ValueError, simulate_circuit, w, circuit)
-    
 
     def test_invalid_basis_state(self):
         circuit = CliffordTCircuit(8, [])
         w = "010a0100"
         self.assertRaises(ValueError, simulate_circuit, w, circuit)
-    
 
     def test_zero(self):
         circuit = CliffordTCircuit(8, [])
@@ -42,14 +50,16 @@ class QlitPythonTests(unittest.TestCase):
                     expected = 1
                 case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
-
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
     def test_imaginary(self):
-        circuit = CliffordTCircuit(8, [
-            CliffordTGate.H(0),
-            CliffordTGate.S(0),
-        ])
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+            ],
+        )
 
         for i in range(0b0000_0000, 0b1111_1111 + 1):
             w = format(i, "08b")
@@ -61,16 +71,18 @@ class QlitPythonTests(unittest.TestCase):
                     expected = 1j / sqrt(2)
                 case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
-
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
     def test_flipped(self):
-        circuit = CliffordTCircuit(8, [
-            CliffordTGate.H(0),
-            CliffordTGate.S(0),
-            CliffordTGate.S(0),
-            CliffordTGate.H(0),
-        ])
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(0),
+                CliffordTGate.H(0),
+            ],
+        )
 
         for i in range(0b0000_0000, 0b1111_1111 + 1):
             w = format(i, "08b")
@@ -80,14 +92,16 @@ class QlitPythonTests(unittest.TestCase):
                     expected = 1
                 case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
-
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
     def test_bell_state(self):
-        circuit = CliffordTCircuit(8, [
-            CliffordTGate.H(0),
-            CliffordTGate.Cnot(0, 1),
-        ])
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(0, 1),
+            ],
+        )
 
         for i in range(0b0000_0000, 0b1111_1111 + 1):
             w = format(i, "08b")
@@ -97,62 +111,73 @@ class QlitPythonTests(unittest.TestCase):
                     expected = 1 / sqrt(2)
                 case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
-
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
     def test_larger_clifford_circuit(self):
-        circuit = CliffordTCircuit(8, [
-            CliffordTGate.H(1),
-            CliffordTGate.H(0),
-            CliffordTGate.S(2),
-            CliffordTGate.H(3),
-            CliffordTGate.S(1),
-            CliffordTGate.S(0),
-            CliffordTGate.Cnot(2, 3),
-            CliffordTGate.S(1),
-            CliffordTGate.H(0),
-            CliffordTGate.S(3),
-            CliffordTGate.Cnot(1, 0),
-            CliffordTGate.S(3),
-            CliffordTGate.H(1),
-            CliffordTGate.S(3),
-            CliffordTGate.S(1),
-            CliffordTGate.S(3),
-            CliffordTGate.H(1),
-            CliffordTGate.Cnot(3, 2),
-            CliffordTGate.H(1),
-            CliffordTGate.Cnot(3, 1),
-        ])
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(2),
+                CliffordTGate.H(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(0),
+                CliffordTGate.Cnot(2, 3),
+                CliffordTGate.S(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(3),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.S(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 2),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 1),
+            ],
+        )
 
         for i in range(0b0000_0000, 0b1111_1111 + 1):
             w = format(i, "08b")
             result = simulate_circuit(w, circuit)
             match i:
-                case 0b0000_0000 | 0b0100_0000 | 0b1100_0000 | 0b0011_0000 | 0b0111_0000 | 0b1011_0000:
+                case (
+                    0b0000_0000
+                    | 0b0100_0000
+                    | 0b1100_0000
+                    | 0b0011_0000
+                    | 0b0111_0000
+                    | 0b1011_0000
+                ):
                     expected = 1j / sqrt(8)
                 case 0b1000_0000 | 0b1111_0000:
                     expected = -1j / sqrt(8)
-                case _: 
+                case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
-
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
     def test_larger_circuit(self):
-        circuit = CliffordTCircuit(8, [
-            CliffordTGate.T(0),
-            CliffordTGate.H(1),
-            CliffordTGate.S(1),
-            CliffordTGate.H(3),
-            CliffordTGate.H(0),
-            CliffordTGate.S(0),
-            CliffordTGate.S(1),
-            CliffordTGate.S(2),
-            CliffordTGate.T(1),
-            CliffordTGate.H(0),
-            CliffordTGate.Cnot(1, 0),
-            CliffordTGate.T(0),
-            CliffordTGate.S(3),
-        ])
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.T(0),
+                CliffordTGate.H(1),
+                CliffordTGate.S(1),
+                CliffordTGate.H(3),
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(1),
+                CliffordTGate.S(2),
+                CliffordTGate.T(1),
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.T(0),
+                CliffordTGate.S(3),
+            ],
+        )
 
         for i in range(0b0000_0000, 0b1111_1111 + 1):
             w = format(i, "08b")
@@ -174,7 +199,353 @@ class QlitPythonTests(unittest.TestCase):
                     expected = -sqrt(0.125) * 1j
                 case _:
                     expected = 0
-            self.assertAlmostEqual(result, expected)
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+
+class GpuTests(unittest.TestCase):
+    def test_mismatched_qubit_number(self):
+        circuit = CliffordTCircuit(8, [])
+        w = "000000000"
+        self.assertRaises(ValueError, simulate_circuit_gpu, w, circuit)
+
+    def test_invalid_basis_state(self):
+        circuit = CliffordTCircuit(8, [])
+        w = "010a0100"
+        self.assertRaises(ValueError, simulate_circuit_gpu, w, circuit)
+
+    def test_zero(self):
+        circuit = CliffordTCircuit(8, [])
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case 0b0000_0000:
+                    expected = 1
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_imaginary(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case 0b0000_0000:
+                    expected = 1 / sqrt(2)
+                case 0b1000_0000:
+                    expected = 1j / sqrt(2)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_flipped(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(0),
+                CliffordTGate.H(0),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case 0b1000_0000:
+                    expected = 1
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_bell_state(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(0, 1),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case 0b0000_0000 | 0b1100_0000:
+                    expected = 1 / sqrt(2)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_larger_clifford_circuit(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(2),
+                CliffordTGate.H(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(0),
+                CliffordTGate.Cnot(2, 3),
+                CliffordTGate.S(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(3),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.S(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 2),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 1),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case (
+                    0b0000_0000
+                    | 0b0100_0000
+                    | 0b1100_0000
+                    | 0b0011_0000
+                    | 0b0111_0000
+                    | 0b1011_0000
+                ):
+                    expected = 1j / sqrt(8)
+                case 0b1000_0000 | 0b1111_0000:
+                    expected = -1j / sqrt(8)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_larger_circuit(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.T(0),
+                CliffordTGate.H(1),
+                CliffordTGate.S(1),
+                CliffordTGate.H(3),
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(1),
+                CliffordTGate.S(2),
+                CliffordTGate.T(1),
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.T(0),
+                CliffordTGate.S(3),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_gpu(w, circuit)
+            match i:
+                case 0b0000_0000 | 0b1101_0000:
+                    expected = 0.25 + 0.25j
+                case 0b1000_0000:
+                    expected = sqrt(0.125)
+                case 0b0100_0000:
+                    expected = -sqrt(0.125)
+                case 0b1100_0000:
+                    expected = 0.25 - 0.25j
+                case 0b0001_0000:
+                    expected = -0.25 + 0.25j
+                case 0b1001_0000:
+                    expected = sqrt(0.125) * 1j
+                case 0b0101_0000:
+                    expected = -sqrt(0.125) * 1j
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+
+class HybridTests(unittest.TestCase):
+    def test_mismatched_qubit_number(self):
+        circuit = CliffordTCircuit(8, [])
+        w = "000000000"
+        self.assertRaises(ValueError, simulate_circuit_hybrid, w, circuit)
+
+    def test_invalid_basis_state(self):
+        circuit = CliffordTCircuit(8, [])
+        w = "010a0100"
+        self.assertRaises(ValueError, simulate_circuit_hybrid, w, circuit)
+
+    def test_zero(self):
+        circuit = CliffordTCircuit(8, [])
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case 0b0000_0000:
+                    expected = 1
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_imaginary(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case 0b0000_0000:
+                    expected = 1 / sqrt(2)
+                case 0b1000_0000:
+                    expected = 1j / sqrt(2)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_flipped(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(0),
+                CliffordTGate.H(0),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case 0b1000_0000:
+                    expected = 1
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_bell_state(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(0, 1),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case 0b0000_0000 | 0b1100_0000:
+                    expected = 1 / sqrt(2)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_larger_clifford_circuit(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.H(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(2),
+                CliffordTGate.H(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(0),
+                CliffordTGate.Cnot(2, 3),
+                CliffordTGate.S(1),
+                CliffordTGate.H(0),
+                CliffordTGate.S(3),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.S(3),
+                CliffordTGate.S(1),
+                CliffordTGate.S(3),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 2),
+                CliffordTGate.H(1),
+                CliffordTGate.Cnot(3, 1),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case (
+                    0b0000_0000
+                    | 0b0100_0000
+                    | 0b1100_0000
+                    | 0b0011_0000
+                    | 0b0111_0000
+                    | 0b1011_0000
+                ):
+                    expected = 1j / sqrt(8)
+                case 0b1000_0000 | 0b1111_0000:
+                    expected = -1j / sqrt(8)
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
+
+    def test_larger_circuit(self):
+        circuit = CliffordTCircuit(
+            8,
+            [
+                CliffordTGate.T(0),
+                CliffordTGate.H(1),
+                CliffordTGate.S(1),
+                CliffordTGate.H(3),
+                CliffordTGate.H(0),
+                CliffordTGate.S(0),
+                CliffordTGate.S(1),
+                CliffordTGate.S(2),
+                CliffordTGate.T(1),
+                CliffordTGate.H(0),
+                CliffordTGate.Cnot(1, 0),
+                CliffordTGate.T(0),
+                CliffordTGate.S(3),
+            ],
+        )
+
+        for i in range(0b0000_0000, 0b1111_1111 + 1):
+            w = format(i, "08b")
+            result = simulate_circuit_hybrid(w, circuit)
+            match i:
+                case 0b0000_0000 | 0b1101_0000:
+                    expected = 0.25 + 0.25j
+                case 0b1000_0000:
+                    expected = sqrt(0.125)
+                case 0b0100_0000:
+                    expected = -sqrt(0.125)
+                case 0b1100_0000:
+                    expected = 0.25 - 0.25j
+                case 0b0001_0000:
+                    expected = -0.25 + 0.25j
+                case 0b1001_0000:
+                    expected = sqrt(0.125) * 1j
+                case 0b0101_0000:
+                    expected = -sqrt(0.125) * 1j
+                case _:
+                    expected = 0
+            self.assertAlmostEqual(result, expected, delta=DELTA)
 
 
 if __name__ == "__main__":
