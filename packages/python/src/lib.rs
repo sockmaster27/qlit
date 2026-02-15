e00712e05ec70b8610862ac632db2dcd27b024ce
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2026-02-15 17:35:50 +0100

    Explicitly enable/disable from_py_object

--- packages/python/src/lib.rs
+++ packages/python/src/lib.rs
@@ -8,7 +8,7 @@ use rayon::ThreadPoolBuilder;
 
 use pyo3::conversion::FromPyObject;
 
-#[pyclass]
+#[pyclass(from_py_object)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[pyo3(name = "CliffordTGate")]
 // For now, we unfortunately have to duplicate this
@@ -77,7 +77,7 @@ impl From<CliffordTGate> for PyCliffordTGate {
     }
 }
 
-#[pyclass]
+#[pyclass(skip_from_py_object)]
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[pyo3(name = "CliffordTCircuit")]
 struct PyCliffordTCircuit(CliffordTCircuit);
