c42f451df0009499b82d51425319e6fc59a48dd7
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2026-08-21 11:21:39 +0200

    Split randomized tests into CPU, GPU and hybrid components (#64)
    * --pytest
    
    * Install qiskit
    
    * Install qiskit harder
    
    * Remove qiskit install
    
    * Update maturin CI version
    
    * Revert uv lock change
    
    * Install mesa-utils on Linux+musl
    
    * Move mesa installation into runtimes
    
    * apk add
    
    * Install mesa-utils on arch
    
    * Windows num threads
    
    * Revert
    
    * Split randomized tests into CPU, GPU and hybrid components

--- test_script.py
+++ test_script.py
@@ -1,4 +1,9 @@
 import subprocess
 
+import pytest
+
+
+# TODO: Make this marking more granular. Many tests in here do not depend on GPU
+@pytest.mark.gpu
 def test_cargo():
     assert subprocess.call(["cargo", "test"]) == 0, "Cargo tests failed"
